68729c24cda3d70cdcf92784b8f1cbe71605d5dd
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-07-04 17:00:35 +0900

    내용볼것
    1. 터치시 알 흔들기 효과
    2. 옵션 디자인 적용
    3. 글꼴 추가
    4. 팝업 창 디자인

---
+++ _scripts/EggEvt.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EggEvt : MonoBehaviour {
+
+    public GameObject eggImg,touchArea;
+    private Vector3 eggImgPos;
+    int rotateInt = 5;
+
+
+    
+    public void shakeEgg() {
+        rotateInt = 5;
+        StopCoroutine("rotateImg");
+        StartCoroutine("rotateImg");
+    }
+
+    IEnumerator rotateImg()
+    {
+        touchArea.SetActive(false);
+        while (rotateInt > -6)
+        {
+            transform.Rotate(0, 0, rotateInt);
+            rotateInt = rotateInt - 2;
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        while (rotateInt < 6)
+        {
+            transform.Rotate(0, 0, rotateInt);
+            rotateInt = rotateInt + 2;
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        while (rotateInt > -6)
+        {
+            transform.Rotate(0, 0, rotateInt);
+            rotateInt = rotateInt - 2 ;
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        touchArea.SetActive(true);
+
+
+
+    }
+
+
+
+
+    }
+
+
