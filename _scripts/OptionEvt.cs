75a18684a85308d3490ceb75087057e6269c4e29
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-08-14 16:40:31 +0900

    머지때문에 올려야됨

--- _scripts/OptionEvt.cs
+++ _scripts/OptionEvt.cs
@@ -8,29 +8,49 @@ public class OptionEvt : MonoBehaviour {
 	public GameObject option_obj, help_obj;
 	public GameObject GM;
 
-    public AudioSource bgm_title;
+    public AudioSource bgm_title, bgm_first;
     public GameObject BGMmute, SEmute;
     public Sprite[] spr_mute;
 
     //아이디
-
     public Text id_txt;
     
 	void Start () {
 
+        //알게된 정보 : gameobject = GM으로 나오고 go대신 직접 변수쓰면 아래와 같음
+        bgm_first = bgm_first.GetComponent<AudioSource>();
+
         //배경과 효과음 이미지변경
         bgm_title = this.gameObject.GetComponent<AudioSource>();
+        
+        //배경음이 두개라 해야함 (밑에서 mute상태에 따라 켜질예정)
+        GM.GetComponent<OptionEvt>().bgm_title.mute = true;
+
         if (PlayerPrefs.GetInt("soundmute", 0) == 1)
         {
             SEmute.GetComponent<Image>().sprite = spr_mute[1];
-
         }
         if (PlayerPrefs.GetInt("soundBGmute", 0) == 1)
         {
             BGMmute.GetComponent<Image>().sprite = spr_mute[1];
             bgm_title.mute = true;
+            bgm_first.mute = true;
         }
     }
+
+    public void bgmCheck()
+    { //타이틀용 : 배경음이 두개라서 해야함
+        if (PlayerPrefs.GetInt("soundBGmute", 0) == 1)
+        {
+            bgm_title.mute = true;
+        }
+        else
+        {
+            bgm_title.Play();
+            bgm_title.mute = false;
+        }
+    }
+
 	
 	public void optionOpen(){
 		GM.GetComponent<OptionEvt> ().option_obj.SetActive (true);
