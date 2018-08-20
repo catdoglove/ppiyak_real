5207f2466827f18ff5f5953d00d57aabd18a833c
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-08-20 16:53:42 +0900

    효과버그수정

--- _scripts/BookEvt.cs
+++ _scripts/BookEvt.cs
@@ -43,8 +43,7 @@ public class BookEvt : MonoBehaviour {
 		Book_obj.SetActive (true);
 		//여기에 효과코딩
 
-		effectShow_txt.text = GM.GetComponent<ShopEvt> ().BackEffect_str [PlayerPrefs.GetInt ("effect_set", 0)]  + "\n" + GM.GetComponent<ShopEvt> ().BottomEffect_str [PlayerPrefs.GetInt ("beffect_set", 0)]+ "\n" + GM.GetComponent<ShopEvt> ().incubatorEffect_str [PlayerPrefs.GetInt ("ieffect_set", 0)];
-		Debug.Log(GM.GetComponent<ShopEvt> ().BackEffect_str [PlayerPrefs.GetInt ("effect_set", 0)]  + "\n" + GM.GetComponent<ShopEvt> ().BottomEffect_str [PlayerPrefs.GetInt ("beffect_set", 0)]+ "\n" + GM.GetComponent<ShopEvt> ().incubatorEffect_str [PlayerPrefs.GetInt ("ieffect_set", 0)]);
+		effectShow_txt.text = GM.GetComponent<ShopEvt> ().BackEffect_str [PlayerPrefs.GetInt ("backset", 0)]  + "\n" + GM.GetComponent<ShopEvt> ().BottomEffect_str [PlayerPrefs.GetInt ("bottomset", 0)]+ "\n" + GM.GetComponent<ShopEvt> ().incubatorEffect_str [PlayerPrefs.GetInt ("incubatorset", 0)];
 
 		for (int i = 0; i < 20; i++) {
 			GM.GetComponent<BookEvt> ().bookGM_str = "Image (" + i + ")";
