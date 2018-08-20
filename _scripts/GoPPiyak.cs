80d78fb42d03475f050f712ce22cff663d11bbbf
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-08-20 16:12:21 +0900

    피버2배 움직임
    및 메뉴버튼 수정

--- _scripts/GoPPiyak.cs
+++ _scripts/GoPPiyak.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class GoPPiyak : MonoBehaviour {
 
-	public GameObject goPy,goPy_btn;
+	public GameObject goPy,goPy_btn, goPy2;
 	public float moveX,moveY;
 	int beuk;
 	// Use this for initialization
@@ -18,10 +18,13 @@ public class GoPPiyak : MonoBehaviour {
 		if (moveX <= -2.4) {
 			beuk = 1;
 			goPy.transform.rotation = Quaternion.Euler (0,180,0);
-		}else if(moveX >= 2.4){
+            goPy2.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if(moveX >= 2.4){
 			beuk = 0;
 			goPy.transform.rotation = Quaternion.Euler (0,0,0);
-		}
+            goPy2.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
 		if (beuk == 1) {
 			moveX = moveX + 0.01f;
 		} else {
